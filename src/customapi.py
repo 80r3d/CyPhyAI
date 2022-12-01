1a206e0e686416070ca7b17147055a6b76062cd4
Jay Patel <jayjp2214@gmail.com>
2022-12-01 00:39:01 -0800

    updated tes & customapi
    added custom functions to connect and run multiple spheroes simultaneously

--- src/customapi.py
+++ src/customapi.py
@@ -1,5 +1,10 @@
 import time
 from spherov2.types import Color
+from spherov2 import scanner
+from spherov2.sphero_edu import SpheroEduAPI, EventType
+from spherov2.toy.bolt import BOLT
+from spherov2.types import Color
+import threading
 '''
 Define Custom APIs in this file
 '''
@@ -21,3 +26,45 @@ def on_charging(api):
 def on_not_charging(api):
     api.set_main_led(Color(255, 0, 47))
     print('not charging')
+
+def connect_to_bolt(toy,droids):
+    
+    try:
+        with SpheroEduAPI(toy) as droid:
+            print(toy.name + ' started!')
+            loc = [0.00,0.00]
+            action = "up"
+            droid.set_main_led(Color(r=0, g=255, b=0))
+            droids.append(droid)
+            run_bolt(droid)
+            # droid.register_event(EventType.on_charging, customapi.on_charging)
+            # droid.register_event(EventType.on_not_charging, customapi.on_not_charging)
+            # time.sleep(2)           
+            
+            
+    except Exception as e:
+        print('Connection failed with ' + toy.name)
+        print("error :" +str(e))
+        print('Reconnecting to ' + toy.name)
+        toy = scanner.find_toy(toy_name=toy.name, toy_types=[BOLT])
+        connect_to_bolt(toy,droids)
+
+
+        
+def run_bolt(droid):
+    droid.set_heading(0)
+    droid.roll(0,255,2)
+    time.sleep(1)
+    droid.roll(180,255,2)
+
+
+def run_bolts(toys):
+    for toy in toys:
+        run_bolt(toy)
+
+def init_connections(toys,droids):
+    for toy in toys:
+        t = threading.Thread(target=connect_to_bolt, args=(toy,droids))
+        t.start()
+        time.sleep(1)
+        
--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 '''
 Project : CyPhyAI
-Created By : Jay Pate
+Created By : Jay Patel
 '''
 import time
 from spherov2 import scanner
@@ -10,37 +10,17 @@ from spherov2.types import Color
 import customapi
 import threading
 
-toy = scanner.find_toy(toy_name='SB-C54E', toy_types=[BOLT])
-while True:
-    try:
-        with SpheroEduAPI(toy) as droid:
-            print(toy.name + ' started!')
-            loc = [0.00,0.00]
-            action = "up"
-            droid.set_main_led(Color(r=255, g=0, b=0))
-            droid.register_event(EventType.on_charging, customapi.on_charging)
-            droid.register_event(EventType.on_not_charging, customapi.on_not_charging)
-            time.sleep(2)
-            break            
-            
-            
-    except Exception as e:
-        print('Connection failed with ' + toy.name)
-        print("error :" +str(e))
-        print('Reconnecting to ' + toy.name)
-        toy = scanner.find_toy(toy_name=toy.name, toy_types=[BOLT])
-        
-with SpheroEduAPI(toy) as droid:
-    droid.register_event(EventType.on_collision, customapi.on_collision)
-    droid.set_main_led(Color(r=0, g=255, b=0))
-    time.sleep(2)
-    droid.set_main_led(Color(r=0, g=0, b=255))
-    time.sleep(2)
-      
-    droid.spin(180,0.1)
-    droid.set_speed(10000)
-    time.sleep(10)
-    droid.set_speed(0)
-            
+
+toys = scanner.find_toys(toy_names=['SB-0613','SB-3D46','SB-B5BA'], toy_types=[BOLT])
+
+print(toys)     
+
+#==============================================initialize & execute=================================================
+droids = []
+customapi.init_connections(toys,droids)
+
+#================================================================================================
+
+    
 
 	
